ffb03f7fa83d3699eca63dc4f5d1dfa188f40c77
Harshita Ravi <95306077+Harshita1801@users.noreply.github.com>
2025-06-11 12:24:32 +0530

    Update test.java

--- test.java
+++ test.java
@@ -3,6 +3,6 @@ public class test
     public static void main(String args[])
     {
       System.out.println("I am jenkins ");
-      System.out.println("I have updated it "):
+      System.out.println("I have updated it ");
     }
   }
